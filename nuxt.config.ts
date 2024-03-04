c64a1e8e0bb65e6750951017c99e90f0fe8c2037
Sunnalen <sunnalen@mail.ru>
2024-03-04 11:39:27 +0500

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
+  moduls: [
+    ['@pinia/nuxt', {disableVuex: true}]
+  ],
   devtools: { enabled: true }
 })
