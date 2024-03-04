0a1210ed18cb910eb07ef62b647aaf38122385d5
Sunnalen <sunnalen@mail.ru>
2024-03-04 17:12:38 +0500

    connect the library of validation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default ({
-  modules: ['@pinia/nuxt'],
+  modules: [
+    '@pinia/nuxt',
+    '@vee-validate/nuxt',
+  ],
   devtools: { enabled: true }
 })
