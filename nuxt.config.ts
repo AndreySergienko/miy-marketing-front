d12511874064d8e6b1d2ee7f83b8f30ae2954065
Sunnalen <sunnalen@mail.ru>
2024-03-07 19:22:00 +0500

    configuration of vee-validate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-export default ({
+export default defineNuxtConfig ({
   modules: [
     '@pinia/nuxt',
     '@vee-validate/nuxt',
   ],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  veeValidate: {
+    autoImports: true,
+    componentNames: {
+      Form: 'VeeForm',
+      Field: 'VeeField',
+      FieldArray: 'VeeFieldArray',
+      ErrorMessage: 'VeeErrorMessage',
+    },
+  },
 })
