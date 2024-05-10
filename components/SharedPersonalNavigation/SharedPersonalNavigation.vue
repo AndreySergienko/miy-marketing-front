81bbaa46c420c63094cb8369414ebd1068eeac06
Martin <martbelmoaw@gmail.com>
2024-05-10 11:27:51 +0300

    Фундамент страницы списка каналов

--- components/SharedPersonalNavigation/SharedPersonalNavigation.vue
+++ components/SharedPersonalNavigation/SharedPersonalNavigation.vue
@@ -1,44 +1,58 @@
 <template>
-  <div class="navigation">
-    <div class="container">
-      <div class="navigation__inner">
-        <nuxt-link :class="{active: $route.path === '/personal/profile'}" to="/personal/profile">Мой профиль</nuxt-link>
-        <nuxt-link :class="{active: $route.path === '/personal/telegram' || $route.path === '/personal/location'}" to="/personal/telegram">Размещение Тг-каналов</nuxt-link>
-        <nuxt-link :class="{active: $route.path === '/personal/history'}" to="/personal/history">История операций</nuxt-link>
-      </div>
+  <div class="navigation container">
+    <div class="navigation__inner">
+      <nuxt-link
+        :class="{ active: $route.path === '/personal/profile' }"
+        to="/personal/profile"
+      >
+        Мой профиль
+      </nuxt-link>
+      <nuxt-link
+        :class="{
+          active:
+            $route.path === '/personal/telegram' ||
+            $route.path === '/personal/location',
+        }"
+        to="/personal/telegram"
+      >
+        Размещение TG-каналов
+      </nuxt-link>
+      <nuxt-link
+        :class="{ active: $route.path === '/personal/history' }"
+        to="/personal/history"
+      >
+        История операций
+      </nuxt-link>
     </div>
   </div>
 </template>
+
 <style lang="scss" scoped>
- @use 'assets/styles/media';
-  .navigation {
-    border-top: 1px solid var(--color-light-gray);
-    padding-top: var(--ident-xxl);
+@use "assets/styles/media";
 
-    &__inner {
-      display: flex;
-      gap: var(--ident-xxl);
-      font-size: var(--font-size-ml);
-      font-weight: var(--font-weight-medium);
-      padding: 0 var(--ident-lm);
+.navigation {
+  border-top: 1px solid var(--color-light-gray);
+  padding-top: var(--ident-xxl);
 
-      @include media.media-breakpoint-down (sm) {
-        flex-direction: column;
-      }
+  &__inner {
+    display: flex;
+    gap: var(--ident-xxl);
+    font-size: var(--font-size-ml);
+    font-weight: var(--font-weight-medium);
 
-      a {
+    @include media.media-breakpoint-down(sm) {
+      flex-direction: column;
+    }
 
-        &.active {
-          color: var(--color-black);
-          text-decoration-color: var(--color-blue);
-          text-underline-offset: 10px;
-        }
+    a {
+      text-decoration: none;
+      padding-bottom: 8px;
+      color: var(--color-black);
 
-        &:not(.active) {
-          color: var(--color-black);
-          text-decoration: none;
-        }
+      &.active {
+        border-bottom: 2px solid var(--color-blue);
       }
     }
   }
+}
 </style>
--- pages/personal/telegram.vue
+++ pages/personal/telegram.vue
@@ -1,24 +1,16 @@
 <template>
-  
-  <div class="telegram__title">
-    <span class="telegram__title-text">Размещение тг-каналов</span>
-    <SharedButton size="l" color="blue" @click="publish" >Опубликовать Тг-канал</SharedButton>
-  </div>
-  <SharedTgChanel />
+  <div class="telegram container"></div>
 </template>
-<script setup>
-  definePageMeta({
-    layout: 'personal'
-  })
 
-  const router = useRouter()
-  const publish = () => {
-    router.push('/personal/location')
-  }
+<script setup lang="ts">
+definePageMeta({
+  layout: "personal",
+});
 </script>
+
 <style scoped lang="scss">
-  .telegram__title {
-    display: flex;
-    justify-content: space-between;
-  }
+.telegram__title {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
