c3deb4612ab5599e1a4d32a8fd7f006c5ec974e2
Martin <martbelmoaw@gmail.com>
2024-05-25 10:16:48 +0300

    3 и 6 пункты пагинации

--- store/payments/payments.store.ts
+++ store/payments/payments.store.ts
@@ -8,16 +8,17 @@ export const usePaymentsStore = defineStore("global/payments", () => {
   /** Список платежей **/
   const payments = ref<IPaymentsItem[]>([]);
 
+  const pages = ref<number | null>(null);
+
   /** Получить список платежей **/
   async function getAll() {
     try {
       const data = await paymentsService.getAll();
       payments.value = transform(data);
-      console.log(payments.value)
     } catch (e) {
       console.error(e);
     }
   }
 
-  return { payments, getAll };
+  return { payments, pages, getAll };
 });
