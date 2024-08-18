5c96ce9365a9ec3882cd03cc753e5bdce4cccfca
Martin <martbelmoaw@gmail.com>
2024-08-18 21:52:22 +0300

    Добавлена отправка данных на получение

--- api/methods/categories/CategoriesService.ts
+++ api/methods/categories/CategoriesService.ts
@@ -2,8 +2,11 @@ import ApiService from "~/api/core/ApiService";
 import type { TCategoriesApiResponse } from "./categories.types";
 
 export default class CategoriesService extends ApiService {
-  async getAll() {
+  async getAll(dates: string) {
     return await this.$authApi<TCategoriesApiResponse>("categories/all", {
+      params: {
+        dates,
+      },
       onResponse(context) {
         return context.response._data;
       },
--- store/filters/calendar.store.ts
+++ store/filters/calendar.store.ts
@@ -1,9 +1,13 @@
+import { useCategoriesStore } from "../categories/categories.store";
+
 export interface ICalendarRange {
   start: Date;
   end: Date | null;
 }
 
 export const useCalendarStore = defineStore("filters/calendar", () => {
+  const categoriesStore = useCategoriesStore();
+
   const ranges = ref<ICalendarRange[]>([]);
 
   const selectDate = (date: Date): void => {
@@ -34,8 +38,14 @@ export const useCalendarStore = defineStore("filters/calendar", () => {
 
   watch(
     ranges,
-    () => {
-      // TODO: вызвать получение новых данных с бэка, с передачей массива с датами
+    (newRanges) => {
+      const rangesWithTimestamps = newRanges.map((range) => ({
+        start: range.start.getTime(),
+        end: range.end?.getTime() || null,
+      }));
+
+      const strigifiedRanges = JSON.stringify(rangesWithTimestamps);
+      categoriesStore.getAll(btoa(strigifiedRanges));
     },
     {
       deep: true,
