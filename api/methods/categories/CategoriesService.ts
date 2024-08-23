47d8b6fc9458c660303c1431edf2b806fca9a9b0
Martin <martbelmoaw@gmail.com>
2024-08-23 17:10:57 +0300

    Подгружается channels/all

--- api/methods/categories/CategoriesService.ts
+++ api/methods/categories/CategoriesService.ts
@@ -1,18 +1,9 @@
 import ApiService from "~/api/core/ApiService";
 import type { TCategoriesApiResponse } from "./categories.types";
-import { ICalendarRange } from "~/store/filters/calendar.store";
 
 export default class CategoriesService extends ApiService {
-  async getAll(dates: ICalendarRange | null) {
-    let parsedDates = !dates ? undefined : `${dates.start.getTime()}`;
-    if (dates && dates.end) {
-      parsedDates = `${parsedDates},${dates.end.getTime()}`;
-    }
-
+  async getAll() {
     return await this.$authApi<TCategoriesApiResponse>("categories/all", {
-      params: {
-        dates: parsedDates,
-      },
       onResponse(context) {
         return context.response._data;
       },
--- partials/AppTelegram.vue
+++ partials/AppTelegram.vue
@@ -77,179 +77,197 @@
   </div>
 </template>
 <script setup lang="ts">
-    import { useChannelStore } from "~/store/channel/channel.store";
-    import {useCategoriesStore} from "~/store/categories/categories.store";
-    import {useUserStore} from "~/store/user/user.store";
-    import { useAlertStore } from "~/store/alert/alert.store";
-  
+import { useChannelStore } from "~/store/channel/channel.store";
+import { useCategoriesStore } from "~/store/categories/categories.store";
+import { useUserStore } from "~/store/user/user.store";
+import { useAlertStore } from "~/store/alert/alert.store";
+import { useCalendarStore } from "~/store/filters/calendar.store";
 
+const channelStore = useChannelStore();
+const userStore = useUserStore();
 
-      const channelStore = useChannelStore();
-      const userStore = useUserStore()
+const { getAllFormat } = useChannelStore();
+const { formatAll } = toRefs(channelStore);
 
-      const{getAllFormat} = useChannelStore()
-      const {formatAll} = toRefs(channelStore)
+const currentFormat = (formatChannelId: number) => {
+  const format = formatAll.value.find((item) => item.id === formatChannelId);
+  if (!format) return "Нет подходящего формата";
+  return format.value;
+};
 
-      const currentFormat = (formatChannelId:number) => {
-        const format = formatAll.value.find(item => item.id === formatChannelId);
-        if (!format) return 'Нет подходящего формата'
-        return format.value;
-      }
+/** pagination **/
+const { paginationQuery, incrementPage } = usePagination();
+/** categories **/
+const categoriesStore = useCategoriesStore();
+const calendarStore = useCalendarStore();
+const { range } = storeToRefs(calendarStore);
 
-      /** pagination **/
-      const { paginationQuery, incrementPage } = usePagination()
-      /** categories **/
-      const categoriesStore = useCategoriesStore()
+const { permissions } = storeToRefs(userStore);
+const { isLoading } = storeToRefs(channelStore);
+const { getQueryCategories, activeCategories } = storeToRefs(categoriesStore);
+const { channelsAll } = storeToRefs(channelStore);
+const alertStore = useAlertStore();
+const {
+  clearInfoChannel,
+  setInfoChannel,
+  slotId,
+  times,
+  activeSlots,
+  dateIdx,
+  days,
+} = useBuyChannel();
 
+const buy = async () => {
+  if (!slotId.value) {
+    alertStore.showError({ title: "Укажите время" });
+    return;
+  }
+  if (!dateIdx.value && +dateIdx.value !== 0) {
+    console.log(dateIdx.value);
 
-      const { permissions } = storeToRefs(userStore)
-      const { isLoading } = storeToRefs(channelStore)
-      const { getQueryCategories, activeCategories } = storeToRefs(categoriesStore)
-      const { channelsAll } = storeToRefs(channelStore);
-      const alertStore = useAlertStore()
-      const { clearInfoChannel, setInfoChannel, slotId, times, activeSlots, dateIdx, days } = useBuyChannel()
+    alertStore.showError({ title: "Укажите дату" });
+    return;
+  }
+  await channelStore.buy(+slotId.value, +dateIdx.value);
+  clearInfoChannel();
+};
 
-      const buy = async () => {
-        if (!slotId.value) {
-          alertStore.showError({ title: 'Укажите время' })
-          return
-        };
-        if (!dateIdx.value && +dateIdx.value !== 0) {
-          console.log(dateIdx.value);
+async function fetchChannels(isMounted?: boolean) {
+  const fullPath = getQueryCategories.value
+    ? paginationQuery.value + "&" + getQueryCategories.value
+    : paginationQuery.value;
+  await channelStore.getAll({ dates: range.value, url: fullPath, isMounted });
+}
 
-          alertStore.showError({ title: 'Укажите дату' })
-          return
-        }
-        await channelStore.buy(+slotId.value, +dateIdx.value)
-        clearInfoChannel()
-      }
+watch(paginationQuery, async () => await fetchChannels());
 
-      async function fetchChannels(isMounted?: boolean) {
-        const fullPath = getQueryCategories.value ? paginationQuery.value + '&' + getQueryCategories.value  : paginationQuery.value
-        await channelStore.getAll({ url: fullPath, isMounted })
-      }
+watch(activeCategories, async () => await fetchChannels(true), { deep: true });
 
-      watch(paginationQuery, async () => await fetchChannels())
+useAsyncData(() =>
+  channelStore.getAll({
+    dates: range.value,
+    url: paginationQuery.value,
+    isMounted: true,
+  })
+);
 
-      watch(activeCategories, async () => await fetchChannels(true), { deep: true })
+onMounted(() => {
+  getAllFormat();
+});
 
-      useAsyncData(() => channelStore.getAll({ url: paginationQuery.value, isMounted: true }))
-
-      onMounted(() => {
-        getAllFormat()
-      })
+watch(range, () => fetchChannels(), { deep: true });
 </script>
 
 <style scoped lang="scss">
-   @use 'assets/styles/media';
-  .tg {
-    padding-top: 150px;
-    margin-top: -50px;
-    -webkit-background-clip: content-box;
-    background-clip: content-box;
+@use "assets/styles/media";
+.tg {
+  padding-top: 150px;
+  margin-top: -50px;
+  -webkit-background-clip: content-box;
+  background-clip: content-box;
 
+  margin-bottom: var(--indent-4xl);
+
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__text {
+    width: 50%;
+    text-align: center;
     margin-bottom: var(--indent-4xl);
 
-    &__inner {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    &__text {
-      width: 50%;
-      text-align: center;
-      margin-bottom: var(--indent-4xl);
-
-      @include media.media-breakpoint-down(sm) {
-        width: 95%;
-      }
+    @include media.media-breakpoint-down(sm) {
+      width: 95%;
     }
   }
+}
 
-  .card__list {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: var(--indent-2xl);
+.card__list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: var(--indent-2xl);
 
-    @include media.media-breakpoint-down(xl) {
-      grid-template-columns: repeat(3, 1fr);
-    }
+  @include media.media-breakpoint-down(xl) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 
-    @include media.media-breakpoint-down(md) {
-      padding: 0 var(--indent-2xl);
-      grid-template-columns: repeat(2, 1fr);
-    }
+  @include media.media-breakpoint-down(md) {
+    padding: 0 var(--indent-2xl);
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @include media.media-breakpoint-down(sm) {
+    padding: 0 150px;
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+.more {
+  margin-bottom: 100px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &__text {
+    font-size: var(--font-size-m);
+    font-weight: var(--font-weight-medium);
+    margin-right: var(--indent-s);
 
     @include media.media-breakpoint-down(sm) {
-      padding: 0 150px;
-      grid-template-columns: repeat(1, 1fr);
+      font-size: var(--font-size-s);
     }
   }
 
-   .more {
-     margin-bottom: 100px;
+  &__icon {
+    font-size: var(--font-size-m);
+  }
+}
 
-     display: flex;
-     justify-content: center;
-     align-items: center;
-     cursor: pointer;
+.modal-telegram {
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 
-     &__text {
-       font-size: var(--font-size-m);
-       font-weight: var(--font-weight-medium);
-       margin-right: var(--indent-s);
+  &__btn {
+    margin: 0 auto;
+    width: 150px;
+  }
+}
 
-       @include media.media-breakpoint-down(sm) {
-         font-size: var(--font-size-s);
-       }
-     }
+.action__button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-     &__icon {
-       font-size: var(--font-size-m);
-     }
-   }
+  width: 100%;
+  padding: var(--indent-m) 0;
+  gap: var(--indent-m);
 
-   .modal-telegram {
-     width: 500px;
-     display: flex;
-     flex-direction: column;
-     gap: 20px;
+  font-size: var(--font-size-m);
 
-     &__btn {
-       margin: 0 auto;
-       width: 150px;
-     }
-   }
+  border: 1px solid transparent;
+  border-radius: 10px;
+  color: var(--color-white);
+  background-color: var(--color-blue);
 
-   .action__button {
-     display: flex;
-     justify-content: center;
-     align-items: center;
+  @include media.media-breakpoint-down(sm) {
+    font-size: var(--font-size-s);
+    gap: var(--indent-s);
+  }
 
-     width: 100%;
-     padding: var(--indent-m) 0;
-     gap: var(--indent-m);
+  &-icon {
+    font-size: var(--font-size-m);
 
-     font-size: var(--font-size-m);
-
-     border: 1px solid transparent;
-     border-radius: 10px;
-     color: var(--color-white);
-     background-color: var(--color-blue);
-
-     @include media.media-breakpoint-down(sm) {
-       font-size: var(--font-size-s);
-       gap: var(--indent-s);
-     }
-
-     &-icon {
-       font-size: var(--font-size-m);
-
-       @include media.media-breakpoint-down(sm) {
-         font-size: var(--font-size-s);
-       }
-     }
-   }
+    @include media.media-breakpoint-down(sm) {
+      font-size: var(--font-size-s);
+    }
+  }
+}
 </style>
