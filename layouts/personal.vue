de2cf0a5a26658b04b0f342d445ade5563c49812
Martin <martbelmoaw@gmail.com>
2024-09-14 10:05:29 +0300

    feat: change layout

--- layouts/personal.vue
+++ layouts/personal.vue
@@ -1,19 +1,13 @@
 <template>
-    <AuthStateController>
-      <template #default="{ isAuth, logout }">
-        <AppHeader :is-auth="isAuth" :logout="logout" />
-      </template>
-    </AuthStateController>
+  <AppHeader />
   <SharedPersonalNavigation />
   <div>
-    <slot/>
+    <slot />
   </div>
   <AppFooter />
 </template>
 
 <script setup>
-  import AppHeader from '../components/AppHeader/AppHeader.vue';
-  import AppFooter from '../components/AppFooter/AppFooter.vue';
-  import SharedPersonalNavigation from '../components/SharedPersonalNavigation/SharedPersonalNavigation.vue';
-  import AuthStateController from "~/controllers/AuthStateController/AuthStateController.vue";
+import SharedPersonalNavigation from "../components/SharedPersonalNavigation/SharedPersonalNavigation.vue";
+import AuthStateController from "~/controllers/AuthStateController/AuthStateController.vue";
 </script>
