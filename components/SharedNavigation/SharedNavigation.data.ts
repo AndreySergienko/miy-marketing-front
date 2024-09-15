dfe1f3205bb4ef65ba6b030fb8a171e4ed8756f9
Martin <martbelmoaw@gmail.com>
2024-09-15 11:02:37 +0300

    refactor: remove unused components

--- components/SharedNavigation/SharedNavigation.data.ts
+++
@@ -1,8 +0,0 @@
-import { INavItem } from "../SharedNavigation/SharedNavigation.type";
-
-export const navList: INavItem[] = [
-  {link:'benefit', text: 'Преимущества'},
-  {link: 'telegram', text: 'Каналы'},
-  {link: 'faq', text: 'FAQ'},
-  {link: 'contact', text: 'Контакты'},
-]
--- controllers/AuthStateController/AuthStateController.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <slot :is-auth="authStore.isAuth" :logout="authStore.logout" />
-</template>
-
-<script setup lang="ts">
-import {useAuthStore} from "~/store/auth/auth.store";
-
-const authStore = useAuthStore()
-</script>
