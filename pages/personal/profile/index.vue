109636bb45da72e7b02099bd0982223941303dad
Martin <martbelmoaw@gmail.com>
2024-09-15 15:23:09 +0300

    refactor: composable for form

--- pages/personal/profile/index.vue
+++ pages/personal/profile/index.vue
@@ -104,9 +104,7 @@
 </template>
 
 <script setup lang="ts">
-import { object, string } from "yup";
 import { useUserStore } from "~/store/user/user.store";
-import { validateInn } from "~/utils/validator.ts/inn.validator";
 
 definePageMeta({
   layout: "personal",
@@ -115,47 +113,7 @@ definePageMeta({
 const userStore = useUserStore();
 const { user } = storeToRefs(userStore);
 
-const passwordRules = string()
-  .min(10, rules.minPassword)
-  .max(40, rules.maxPassword)
-  .matches(/^(?=.*[0-9])/, rules.number)
-  .matches(/^(?=.*[A-Z])/, rules.letter)
-  .matches(/^(?=.*[!@#$%^&*])/, rules.symbol)
-  .label("");
-
-const { meta, values } = useForm({
-  validationSchema: object({
-    fio: string().required(rules.required).label(""),
-    email: string().email(rules.email).required(rules.required).label(""),
-    inn: string()
-      .required(rules.required)
-      .test("validateInn", rules.inn, validateInn)
-      .label(""),
-
-    bankName: string().label(""),
-    bankBik: string().label(""),
-    bankCorAccount: string().label(""),
-    bankCurAccount: string().label(""),
-
-    password: passwordRules,
-    newPassword: passwordRules,
-    repeatPassword: passwordRules,
-  }),
-  initialValues: {
-    fio: user.value?.fio,
-    email: user.value?.email,
-    inn: user.value?.inn,
-
-    bankName: user.value?.bank?.name,
-    bankBik: user.value?.bank?.bik,
-    bankCorAccount: user.value?.bank?.correspondentAccount,
-    bankCurAccount: user.value?.bank?.currentAccount,
-
-    password: "",
-    newPassword: "",
-    repeatPassword: "",
-  },
-});
+const { meta, values } = useProfileForm(user);
 
 const handleSave = async () => {
   if (!meta.valid) return;
