68a99083713668b94804a083554b4e9699eec23a
Martin <martbelmoaw@gmail.com>
2024-09-22 15:45:29 +0300

    feat: add card

--- pages/personal/telegram.vue
+++ pages/personal/telegram.vue
@@ -8,6 +8,7 @@
       :subscribers="1500"
       :is-active="false"
     />
+    <ChannelAdd />
   </div>
 </template>
 
@@ -27,5 +28,22 @@ const cannotCreate = computed(() => !permissions.value.CAN_PUBLIC_CHANNEL);
 <style scoped lang="scss">
 @use "assets/styles/media";
 .telegram-page {
+  margin-top: 24px;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 20px;
+
+  @include media.media-breakpoint-up(md) {
+    margin-top: 21px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @include media.media-breakpoint-up(lm) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @include media.media-breakpoint-up(xxl) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 }
 </style>
