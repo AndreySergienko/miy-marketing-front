2072b9081990396cc5d2585057be08c7b5340273
Martin <martbelmoaw@gmail.com>
2024-09-26 00:59:38 +0300

    feat: add ui for edit dates

---
+++ components/TelegramEditDates/Chip/Chip.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chip">
+    <span class="chip__label">{{ date }}</span>
+    <NuxtIcon
+      class="chip__icon"
+      name="file-input-remove"
+      filled
+      @click="$emit('remove')"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IChipProps, IChipEmits } from "./Chip.types";
+
+defineProps<IChipProps>();
+defineEmits<IChipEmits>();
+</script>
+
+<style scoped lang="scss" src="./Chip.scss"></style>
--- components/TelegramEditDates/TelegramEditDates.vue
+++ components/TelegramEditDates/TelegramEditDates.vue
@@ -4,6 +4,44 @@
       Даты для публикаций
       <DefaultTooltip> Для выбора доступно до 12 дат </DefaultTooltip>
     </h2>
+    <div class="telegram-edit-dates__content">
+      <header class="calendar__header">
+        <h2 class="calendar__header-date">Август 2024</h2>
+        <div class="calendar__header-buttons">
+          <NuxtIcon name="default-calendar-arrow" filled />
+          <NuxtIcon
+            class="calendar__header-buttons-right"
+            name="default-calendar-arrow"
+            filled
+          />
+        </div>
+      </header>
+      <main class="calendar__main">
+        <div class="calendar__main-titles">
+          <span
+            v-for="day in weekDays"
+            :key="day"
+            class="calendar__main-titles-item"
+          >
+            {{ day }}
+          </span>
+        </div>
+        <div class="calendar__main-days">
+          <span v-for="day in 31" :key="day" class="calendar__main-days-item">
+            {{ day }}
+          </span>
+        </div>
+      </main>
+      <div class="calendar__divider"></div>
+      <footer class="calendar__footer">
+        <h3 class="calendar__footer-title">Выбранные даты</h3>
+        <div class="calendar__footer-chips">
+          <TelegramEditDatesChip date="11.08.2024" />
+          <TelegramEditDatesChip date="11.08.2024" />
+          <TelegramEditDatesChip date="11.08.2024" />
+        </div>
+      </footer>
+    </div>
   </section>
 </template>
 
@@ -19,6 +57,8 @@ const props = defineProps<ITelegramEditDatesProps>();
 const { dates } = toRefs(props);
 
 const emit = defineEmits<ITelegramEditDatesEmits>();
+
+const weekDays = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
 
 const { meta, values } = useForm({
   validationSchema: object({
