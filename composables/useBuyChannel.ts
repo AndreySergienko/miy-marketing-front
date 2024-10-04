64d2d6319a721393222a771abb4978f3b2951bbf
Martin <martbelmoaw@gmail.com>
2024-10-04 11:45:23 +0300

    fix: show all channels

--- composables/useBuyChannel.ts
+++ composables/useBuyChannel.ts
@@ -1,37 +1,41 @@
-import type { ISlot } from '~/api/methods/channels/channels.types'
-import type { ISharedSelectOption } from '~/components/SharedSelect/SharedSelect.types'
-import { convertUtcDateToTime } from '~/utils/date'
+import type {
+  IGetAll,
+  IGetAllDate,
+  IGetAllDateSlot,
+} from "~/api/methods/channels/channels.types";
+import type { ISharedSelectOption } from "~/components/SharedSelect/SharedSelect.types";
 
 export function useBuyChannel() {
-  const activeSlots = ref<ISlot[]>([])
-  const activeDays = ref<string[]>([])
-  const slotId = ref<string>('')
-  const dateIdx = ref<string>('')
+  const activeSlots = ref<IGetAllDateSlot[]>([]);
+  const activeDays = ref<IGetAllDate[]>([]);
 
-  const setInfoChannel = (slots: ISlot[], days: string[]) => {
-    activeSlots.value = slots
-    activeDays.value = days
-  }
+  const slotId = ref<string>("");
+  const dateIdx = ref<string>("");
+
+  const setInfoChannel = (channel: IGetAll, date: IGetAllDate) => {
+    activeSlots.value = date.slots;
+    activeDays.value = channel.channelDates;
+  };
 
   const times = computed<ISharedSelectOption[]>(() => {
     return activeSlots.value.map((slot) => {
       return {
-        title: String(convertUtcDateToTime(+slot.timestamp)),
+        title: slot.timestamp,
         value: String(slot.id),
-      }
-    })
-  })
+      };
+    });
+  });
 
   const days = computed<ISharedSelectOption[]>(() => {
-    return activeDays.value.map((day, idx) => {
+    return activeDays.value.map((day) => {
       return {
-        title: day,
-        value: String(idx),
-      }
-    })
-  })
+        title: day.date,
+        value: String(day.id),
+      };
+    });
+  });
 
-  const clearInfoChannel = () => (activeSlots.value = [])
+  const clearInfoChannel = () => (activeSlots.value = []);
 
   return {
     dateIdx,
@@ -41,5 +45,5 @@ export function useBuyChannel() {
     times,
     days,
     clearInfoChannel,
-  }
+  };
 }
