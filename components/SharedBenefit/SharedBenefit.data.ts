9f7b0bc02c8da7be9a598793486c9b653c8dd40e
Sunnalen <sunnalen@mail.ru>
2025-02-23 11:41:52 +0500

    fix a content of the site

--- components/SharedBenefit/SharedBenefit.data.ts
+++ components/SharedBenefit/SharedBenefit.data.ts
@@ -2,6 +2,6 @@ import { IBenefitItem } from "./SharedBenefit.type";
 
 export const benefitList: IBenefitItem[]= [
   {text: '“Налоговый кэшбэк”*', number: '1'},
-  {text: '0% комиссии за рекламные интеграции', number:'2'},
+  {text: 'возможность выбора порядка публикации интеграций: автоматический или уведомительный', number:'2'},
   {text: 'Гарантия исполнения обязательства или возврат денежных средств', number:'3'},
 ]
